2c087d51ef7b55a6b52c207b689ceddfe62a77ee
MaciejP <m.ponicki1995@gmail.com>
2024-09-30 18:07:30 +0200

    check if backend works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    // Wykonaj zapytanie do serwera Node.js
+    fetch("https://todoapi-20h6.onrender.com")
+      .then((response) => response.text()) // Oczekuje tekstowej odpowiedzi
+      .then((data) => {
+        setData(data); // Ustaw dane w stanie
+      })
+      .catch((error) => console.error("Błąd:", error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Siemanko deploy test</p>
+        <p>{data}</p>
       </header>
     </div>
   );
